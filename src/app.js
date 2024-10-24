32ed0e1a1ac55c028c5796b307c11f7351321710
Rohan Krishnakumar <rohankrishnakumar@MacBook-Pro-82.hsd1.nj.comcast.net>
2024-10-24 10:34:29 -0400

    Set up app for Render

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ const app = express()
 const hbs = require("hbs")
 const geocode = require("./utils/geocode")
 const forecast = require("./utils/forecast")
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirPath = path.join(__dirname, "../public")
@@ -100,6 +101,6 @@ app.get("*", (req,res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000")
+app.listen(port, () => {
+    console.log("Server is running on port "+ port)
 })
