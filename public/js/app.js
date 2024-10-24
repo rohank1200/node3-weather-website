5746f06dc17d99f9a702cec8da66b92f1c010830
Rohan Krishnakumar <rohankrishnakumar@MacBook-Pro-82.hsd1.nj.comcast.net>
2024-10-24 10:35:39 -0400

    Set up app for Render

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,3 @@
-fetch("http://localhost:3000/weather?address=philadelphia").then((response) => {
-    response.json().then((data) => {
-        if (data.error){
-            console.log(data.error)
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-})
-
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
 const message1 = document.querySelector("#message-1")
@@ -23,7 +12,7 @@ weatherForm.addEventListener("submit", (e) => {
     message1.textContent = "Loading..."
     message2.textContent = ""
 
-    fetch("http://localhost:3000/weather?address="+location).then((response) => {
+    fetch("/weather?address="+location).then((response) => {
         response.json().then((data) => {
             if (data.error){
                 message1.textContent = data.error
